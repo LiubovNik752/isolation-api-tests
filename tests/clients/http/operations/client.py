8d6a7e2f24b79cc70f9a1591a4eff6b432964f8d
Liubov Baturina <lnbaturina@sberbank.ru>
2026-05-01 11:39:22 +0300

    HTTP API-клиенты сервисов. Практическое задание: HTTP API-клиент для operations-service

--- tests/clients/http/operations/client.py
+++ tests/clients/http/operations/client.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 import allure
-from httpx import Response
+from httpx import Response, QueryParams
 
 from tests.clients.http.client import HTTPTestClient, build_http_test_client
 from tests.config import test_settings
@@ -11,41 +11,41 @@ from tests.tools.routes import APITestRoutes
 
 
 class OperationsHTTPTestClient(HTTPTestClient):
-    """ HTTP API-клиент тестового слоя для operations-service."""
-
-    @allure.step("Get operation details")
+    @allure.step("Get operation")
     def get_operation_api(self, operation_id: UUID) -> Response:
-        return self.get(f"{APITestRoutes.GATEWAY}/api/v1/operations/{operation_id}")
+        return self.get(f'{APITestRoutes.OPERATIONS}/{operation_id}')
 
     @allure.step("Get operations")
     def get_operations_api(self, query: GetOperationsQueryTestSchema) -> Response:
         return self.get(
-            f"{APITestRoutes.GATEWAY}/api/v1/operations",
-            params=query.model_dump(by_alias=True, exclude_none=True)
+            APITestRoutes.OPERATIONS,
+            params=QueryParams(**query.model_dump(by_alias=True, exclude_none=True))
         )
 
     def get_operation(self, operation_id: UUID) -> GetOperationResponseTestSchema:
         response = self.get_operation_api(operation_id)
         response.raise_for_status()
-
         return GetOperationResponseTestSchema.model_validate_json(response.text)
 
-    def get_operations(self, user_id: UUID, card_id: UUID | None = None,
-                       account_id: UUID | None = None) -> GetOperationsResponseTestSchema:
-        query_params_schema = GetOperationsQueryTestSchema(
+    def get_operations(
+            self,
+            user_id: UUID,
+            card_id: UUID | None = None,
+            account_id: UUID | None = None,
+    ) -> GetOperationsResponseTestSchema:
+        query = GetOperationsQueryTestSchema(
             user_id=user_id,
             card_id=card_id,
             account_id=account_id
         )
-        response = self.get_operations_api(query=query_params_schema)
+        response = self.get_operations_api(query)
         response.raise_for_status()
-
         return GetOperationsResponseTestSchema.model_validate_json(response.text)
 
 
 def build_operations_http_test_client() -> OperationsHTTPTestClient:
     client = build_http_test_client(
         logger=get_test_logger("OPERATIONS_HTTP_TEST_CLIENT"),
-        config=test_settings.operations_http_client,
+        config=test_settings.operations_http_client
     )
     return OperationsHTTPTestClient(client=client)
