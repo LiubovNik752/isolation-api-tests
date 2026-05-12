db4f9073c2861e44d728165df3db90d3e21166d0
Liubov Baturina <lnbaturina@sberbank.ru>
2026-05-12 20:16:21 +0300

    Сценарные тесты HTTP API

--- tests/suites/http/test_gateway.py
+++ tests/suites/http/test_gateway.py
@@ -15,17 +15,6 @@ from tests.tools.allure import AllureTag, AllureStory, AllureFeature
 @allure.tag(AllureTag.HTTP, AllureTag.GATEWAY_SERVICE)
 @allure.feature(AllureFeature.GATEWAY_SERVICE)
 class TestGatewayHTTP:
-    """
-    Сценарные HTTP тесты gateway-service.
-
-    Этот тестовый класс:
-    - не подготавливает данные;
-    - не управляет моками напрямую;
-    - не содержит логики маршрутизации или агрегации.
-
-    Он описывает сценарии взаимодействия с gateway
-    в условиях полностью детерминированного внешнего мира.
-    """
 
     @allure.story(AllureStory.GET_USER_DETAILS)
     @allure.title("[HTTP] Get user details. User with active credit card account")
@@ -33,14 +22,6 @@ class TestGatewayHTTP:
         self,
         gateway_http_test_client: GatewayHTTPTestClient,
     ):
-        """
-        Сценарий:
-        Пользователь с активным кредитным счётом
-        запрашивает агрегированные данные через gateway-service.
-
-        Управляющая переменная теста — сценарий.
-        Все данные внешнего мира выбираются через него.
-        """
         response = gateway_http_test_client.get_user_details(
             RequestContext(scenario=Scenario.USER_WITH_ACTIVE_CREDIT_CARD_ACCOUNT)
         )
